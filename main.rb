528483363e7be167804511055ea16c0e9ca893f0
prakhar1989 <prakhar1989@gmail.com>
2012-05-18 00:11:26 +0530

    Wohooo! mongo

--- main.rb
+++ main.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
+require 'sinatra/mongo'
 
 configure do
     set :public_folder, File.dirname(__FILE__) + '/static'
+    set :mongo, 'mongo://localhost:27017/blogtest'
 end
+
+puts mongo["testCollection"].insert({"name" => "mongo", "type" => "database", "count" => 1, "info" => { "x" => 203, "y" => "102" }})
 
 get '/' do
     erb :signup
@@ -10,4 +14,9 @@ end
 
 get '/login' do
     erb :login
+end
+
+get '/mongo' do
+    my_array = mongo["testCollection"].find_one
+    my_array.map{ |i| i.to_s }.join(",")
 end
