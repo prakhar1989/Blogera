943d9392cfc349a354e7b41de35f9a2958c7c971
Arjun Variar <arjun.variar@kony.com>
2012-05-17 18:25:20 +0530

    No Need for extra login page

--- main.rb
+++ main.rb
@@ -4,10 +4,10 @@ configure do
     set :public_folder, File.dirname(__FILE__) + '/static'
 end
 
-get '/' do 
-    "<a href='/login'>Login</a>"
-end
+# get '/' do 
+#     "<a href='/login'>Login</a>"
+# end
 
-get '/login' do
+get '/' do
     erb :login
 end
