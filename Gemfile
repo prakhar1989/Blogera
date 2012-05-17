a8720f4b03f096e6b0a46f881b1b4d1c1d8257ea
Arjun Variar <accio.arjun@gmail.com>
2012-05-18 00:06:55 +0530

    Added routing and first insert to Mongo-DB.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
 gem "sinatra"
-gem "sinatra-mongo-config"
+gem "sinatra-mongo"
 gem "mongo"
 gem "bson"
--- main.rb
+++ main.rb
@@ -1,11 +1,17 @@
 require 'sinatra'
+require 'sinatra/mongo'
 
 configure do
     set :public_folder, File.dirname(__FILE__) + '/static'
+    set :mongo, 'mongo://localhost:27017/blogera'
 end
 
 get '/' do
     erb :signup
+end
+
+post '/signup' do
+	mongo["users"].insert({"email"=>params[:email],"password"=>params[:password]})
 end
 
 get '/login' do
