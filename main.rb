5bb396e3f2d21bead198ee66736358fbfa1f0d65
prakhar1989 <prakhar1989@gmail.com>
2012-05-17 17:57:19 +0530

    Added BOotstrap

--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
-get '/' do
-    "Hello world"
+configure do
+    set :public_folder, File.dirname(__FILE__) + '/static'
+end
+
+get '/' do 
+    "<a href='/login'>Login</a>"
+end
+
+get '/login' do
+    erb :login
 end
