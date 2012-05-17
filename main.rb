87536891ec15a2aa0507b2f3c3fcd52d02ce9e22
Arjun Variar <arjun.variar@kony.com>
2012-05-17 18:48:08 +0530

    Default Condition.

--- main.rb
+++ main.rb
@@ -4,6 +4,11 @@ configure do
     set :public_folder, File.dirname(__FILE__) + '/static'
 end
 
+get '/' do
+	# TODO - Unless Already Signed in
+	redirect '/signup'
+end
+
 get '/signup' do
     erb :login
 end
